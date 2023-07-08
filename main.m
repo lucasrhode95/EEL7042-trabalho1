89e718344d262d56a2961731e86a8684699da7ea
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-08 00:35:44 -0300

    Correção da dimensão de algumas matrizes

--- main.m
+++ main.m
@@ -5,7 +5,7 @@ nb  = 5; % número de barras
 nc  = 3; % número de cargas
 ng  = 3; % número de geradores
 wcc = 1; % índice de ponderação
-nl  = 5; % número de linhas
+nl  = 6; % número de linhas
 
 % peso dos cortes de carga ($/MWpu)
 % (problema não deu valores, assumindo peso uniforme = 1)
@@ -25,13 +25,13 @@ Um(3,3) = 1; % carga 3 na barra 3
 
 %% GERADORES
 % limites geradores
-Pmax = [5, 3, 5];
-Pmin = [0, 0, 0];
+PgMax = [5, 3, 5]';
+PgMin = [0, 0, 0]';
 % matriz de incidência de geradores
-Ag = zeros(ng, nb);
+Ag = zeros(nb, ng);
 Ag(1,1) = 1; % gerador 1 na barra 1
-Ag(2,3) = 1; % gerador 2 na barra 3
-Ag(3,4) = 1; % gerador 3 na barra 4
+Ag(3,2) = 1; % gerador 2 na barra 3
+Ag(4,3) = 1; % gerador 3 na barra 4
 
 %% LINHAS
 % impedância das linhas
@@ -42,14 +42,16 @@ X(3, 3) = 0.2100; % de 3 para 5
 X(4, 4) = 0.3360; % de 3 para 4
 X(5, 5) = 0.2520; % de 5 para 4
 X(6, 6) = 0.1260; % de 5 para 1
+Xinv = inv(X);
 % limite de fluxo das linhas
-Tmax = zeros(nl);
+Tmax = zeros(nl, 1);
 Tmax(1) = 1.5; % de 1 para 2
 Tmax(2) = 1.5; % de 2 para 3
 Tmax(3) = 1.5; % de 3 para 5
 Tmax(4) = 1.5; % de 3 para 4
 Tmax(5) = 1.5; % de 5 para 4
 Tmax(6) = 0.6; % de 5 para 1
+Tmin = -Tmax;
 
 % matrix incidência barra-ramo A(barra, linha)
 A = zeros(nb, nl);
@@ -72,5 +74,5 @@ A(4, 5) =  1; % barra 4
 A(1, 6) =  1; % barra 1
 A(5, 6) = -1; % barra 5
 
-%% matriz B
+% matriz B
 B = montarMatrizB(diag(X), A);
