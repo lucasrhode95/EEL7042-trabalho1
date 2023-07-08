1525d48974ad297aa2af1d088904d62541997cf0
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-08 16:13:31 -0300

    Melhorias no método `iteracaoMetodoNewton`

--- helpers_metodo_newton/iteracaoMetodoNewton.m
+++ helpers_metodo_newton/iteracaoMetodoNewton.m
@@ -4,16 +4,13 @@ function DeltaZ = iteracaoMetodoNewton(
     barraVTheta,
     DeltaPd, Pg, Theta,
     mu, Lambda, pi1, pi2, pi3, pi4, pi5, s1, s2, s3, s4, s5,
-    Um, Ag, A, B, Xinv,
-    Winv
+    Um, Ag, A, B, Xinv
 )
     % reconstrução de variáveis auxiliares
     vetorPi = [pi1; pi2; pi3; pi4; pi5];
     vetorS  = [s1; s2; s3; s4; s5];
     Bred    = removeColuna(barraVTheta, B);
     Ared    = removeLinha(barraVTheta, A);
-    ndes    = size(vetorS)(1); % quantidade de restrições de desigualdade
-    e       = ones(ndes, 1);
 
     % matriz hessiana
     W    = montarMatrizW(Um, Ag, Ared, Xinv, Bred, vetorS, vetorPi);
@@ -22,7 +19,7 @@ function DeltaZ = iteracaoMetodoNewton(
     L_u = evaluarGradienteLagrangeano(wcc, Alpha, Um, Lambda, Ag, B, A, Xinv, pi1, pi2, pi3, pi4, pi5);
     G_u = evaluarRestricoesIgualdade(Ag, Pg, Pd0, Um, DeltaPd, Bred, Theta);
     H_u = evaluarRestricoesDesigualdade(Pg, PgMin, Xinv, Ared, Theta, Tmin, PgMax, Tmax, DeltaPd, s1, s2, s3, s4, s5);
-    S_u = diag(vetorS)*vetorPi - mu*e;
+    S_u = vetorS.*vetorPi - mu;
 
     % p(z) + W*dz = 0
     Pz = [
