1d034db4eca6a9300b502df2388900be3cc3295e
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-09 03:38:12 -0300

    beh

--- helpers_MPI/calcularAlfa.m
+++ helpers_MPI/calcularAlfa.m
@@ -2,6 +2,6 @@ function alfa = calcularAlfa(z, deltaZ)
     negativos = z < 0;
     zi        = z(negativos);
     dzi       = deltaZ(negativos);
-    dummy     = min(-zi./dzi);
-    alfa      = min([dummy, 1]);
+    dummy     = -zi./dzi;
+    alfa      = min([dummy; 1]);
 end
