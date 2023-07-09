c976b7a718f9abf35c4be7aae884fc59d566d160
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-08 21:28:22 -0300

    Correção no cálculo do gradiente lagrangeano

--- helpers_evaluar_lagrangeano_e_restricoes/evaluarGradienteLagrangeano.m
+++ helpers_evaluar_lagrangeano_e_restricoes/evaluarGradienteLagrangeano.m
@@ -2,12 +2,12 @@
 % ∇L(u)
 function Lu = evaluarGradienteLagrangeano(
     wcc, Alpha, Um, Lambda,
-    Ag, B, A, Xinv,
+    Ag, Bred, Ared, Xinv,
     pi1, pi2, pi3, pi4, pi5
 )
     Lu = [
-        wcc*Alpha - Um'*Lambda - pi5;
-        -Ag'*Lambda + pi3 - pi1;
-        B*Lambda + A*Xinv*(pi4 - pi2);
+        wcc*Alpha - Um'*Lambda - pi5;         % deltaPd
+        -Ag'*Lambda + pi3 - pi1;              % Pg
+        Bred'*Lambda + Ared*Xinv*(pi4 - pi2); % Theta
     ];
 end
--- helpers_metodo_newton/evaluarPz.m
+++ helpers_metodo_newton/evaluarPz.m
@@ -10,14 +10,14 @@ function Pz = evaluarPz(
     Bred    = removeColuna(barraVTheta, B);
     Ared    = removeLinha(barraVTheta, A);
 
-    L_u = evaluarGradienteLagrangeano(wcc, Alpha, Um, Lambda, Ag, B, A, Xinv, pi1, pi2, pi3, pi4, pi5);
+    L_u = evaluarGradienteLagrangeano(wcc, Alpha, Um, Lambda, Ag, Bred, Ared, Xinv, pi1, pi2, pi3, pi4, pi5);
     G_u = evaluarRestricoesIgualdade(Ag, Pg, Pd0, Um, DeltaPd, Bred, Theta);
     H_u = evaluarRestricoesDesigualdade(Pg, PgMin, Xinv, Ared, Theta, Tmin, PgMax, Tmax, DeltaPd, s1, s2, s3, s4, s5);
     S_u = evaluarGradienteLagrangeanoS(vetorS, vetorPi, mu);
 
     Pz = [
         L_u;
-        removeLinha(barraVTheta, G_u);
+        G_u;
         H_u;
         S_u;
     ];
