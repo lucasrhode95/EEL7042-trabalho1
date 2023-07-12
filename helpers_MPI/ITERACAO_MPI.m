a34aed8f10daa98df39d343a3de8782c1c4e37c4
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-12 01:46:29 -0300

    Atualiza rotina `ITERACAO_MPI`

--- helpers_MPI/ITERACAO_MPI.m
+++ helpers_MPI/ITERACAO_MPI.m
@@ -1,14 +1,5 @@
-u = [
-    DeltaPd; % dimDeltaPd
-    Pg;      % dimPg
-    Theta;   % dimTheta
-];
-
 % contagem das variáveis primais e duais
-dimDeltaPd = size(DeltaPd)(1);
-dimPg      = size(Pg)(1);
-dimTheta   = size(Theta)(1);
-dimU       = dimDeltaPd + dimPg + dimTheta;
+dimU       = size(u)(1);
 dimLambda  = size(Lambda)(1);
 dimPi      = size(vetorPi)(1);
 dimS       = size(vetorS)(1);
@@ -21,12 +12,8 @@ dimVar     = dimU + dimLambda + dimPi + dimS;
 %     S;  % dimS
 % ];
 deltaZ = iteracaoMetodoNewton(
-    wcc, Alpha, Pd0,
-    PgMin, PgMax, Tmin, Tmax,
-    barraVTheta,
-    DeltaPd, Pg, Theta,
-    mu, Lambda, pi1, pi2, pi3, pi4, pi5, s1, s2, s3, s4, s5,
-    Um, Ag, A, B, Xinv
+    Q, Pg, b, Iaux, Mat_hor, Pgsolar, Pd, PgMin, Cap, PgMax,
+    mu, Lambda, pi1, pi2, pi3, pi4, s1, s2, s3, s4
 );
 deltaZ = sigmaInterioridade*deltaZ; % para garantir interioridade, conforme apostila
 
@@ -50,25 +37,17 @@ vetorS  += alfaPrimal*deltaS;
 % atualizar mi
 mu = calcularMu(vetorS, vetorPi, dimVar, betaAceleracao);
 
-% separar/atualizar variáveis originais
-next    = criarIterador(u);
-DeltaPd = next(dimDeltaPd);
-Pg      = next(dimPg);
-Theta   = next(dimTheta);
-
 next = criarIterador(vetorS);
 s1 = next(size(s1)(1));
 s2 = next(size(s2)(1));
 s3 = next(size(s3)(1));
 s4 = next(size(s4)(1));
-s5 = next(size(s5)(1));
 
 next = criarIterador(vetorPi);
 pi1 = next(size(pi1)(1));
 pi2 = next(size(pi2)(1));
 pi3 = next(size(pi3)(1));
 pi4 = next(size(pi4)(1));
-pi5 = next(size(pi5)(1));
 
 % condição de tolerância
 Pz  = evaluarPz(
