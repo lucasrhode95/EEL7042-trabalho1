a627a10443bd83ca6626a9f08329d965d4acbb1e
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-12 02:14:59 -0300

    Corrige montagem da matriz Hessiana

--- helpers_metodo_newton/montarMatrizW.m
+++ helpers_metodo_newton/montarMatrizW.m
@@ -4,24 +4,23 @@
 % Uma grande melhoria de performance seria possível mantendo um cache da matriz
 % W gerada, e somente atualizar as células referentes ao vetorS e vetorPi.
 function W = montarMatrizW(Q, Iaux, Mat_hor, vetorS, vetorPi)
+    nt     = size(Q)(1);
     nigual = size(Mat_hor)(1);
     nvar   = size(Mat_hor)(2);
     ndes   = size(vetorS)(1);
 
-    L_u_u = 2*Q;
-
-    L_u_y = Iaux';
-
-    % Obs: transformamos o problema para que só houvessem PI máx e nenhum PI min
-    L_u_pi = [-eye(nvar,nvar), Mat_hor, eye(nvar,nvar), -Mat_hor];
-
+    L_u_u   = 2*Q;
+    L_u_y   = Iaux';
+    L_u_pi  = [-eye(nvar, nt), Mat_hor', eye(nvar, nt), -Mat_hor'];
     L_u_s   = zeros(nvar, ndes);
+
     L_y_y   = zeros(nigual, nigual);
     L_y_pi  = zeros(nigual, ndes);
     L_y_s   = zeros(nigual, ndes);
+
     L_pi_pi = zeros(ndes, ndes);
-    % L_pi_s  = diag(vetorS);
     L_pi_s  = eye(ndes, ndes);
+
     L_s_pi  = diag(vetorS);
     L_s_s   = diag(vetorPi);
 
