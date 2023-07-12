8426f9aa5e82bbaee1eff356556bdaf2a248f645
Lucas Rhode <lucasrhodefloripabit@gmail.com>
2023-07-12 02:16:59 -0300

    Corige cálculo do gradiente do Lagrangeano

--- helpers_evaluar_lagrangeano_e_restricoes/evaluarGradienteLagrangeano.m
+++ helpers_evaluar_lagrangeano_e_restricoes/evaluarGradienteLagrangeano.m
@@ -1,3 +1,3 @@
 function Lu = evaluarGradienteLagrangeano(Q, Pg, b, Iaux, Mat_hor, Lambda, pi1, pi2, pi3, pi4)
-    Lu = 2*Q*Pg + b + Iaux*Lambda + pi3 - pi1 + Mat_hor*(pi2 - pi4);
+    Lu = 2*Q*Pg + b + Iaux'*Lambda + pi3 - pi1 + Mat_hor'*(pi2 - pi4);
 end
